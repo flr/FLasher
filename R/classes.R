023d8bf72c02fb58af77f1d4e6a8c13fb01cb8ec
Iago Mosqueira <iago@mosqueira.eu>
2015-12-03 14:56:34 +0100

    Added documentation for fwdElement

--- R/classes.R
+++ R/classes.R
@@ -9,46 +9,96 @@
 qlevels <-  c('f', 'catch', 'ssb', 'biomass', 'landings', 'discards', 'tsb', 'rec')
 
 # fwdElement class {{{
+#' A class for the elements to set up a fishery projection.
+#'
+#' The desired targets, limits and time steps used in fishery projections can be 
+#' specified by creating an object of class \code{fwdElement}.
+#' 
+#' [...]
+#' 
+#' @name fwdElement
+#' @rdname fwdElement
+#' @aliases fwdElement fwdElement-methods fwdElement-class
+#' @docType class
+#'
+#' @section Slots:
+#'     \describe{
+#'     \item{element}{The table of values, quantities and time steps (\code{data.frame}).}
+#'     \item{iters}{Values for multiple iterations (\code{array}).}
+#' }
+#'
+#' @section Validity: \describe{
+#'     \item{VALIDITY}{Neque porro quisquam est qui dolorem ipsum.}
+#' }
+#'
+#' @section Accessors:
+#' All slots in the class have accessor and replacement methods defined that
+#' allow retrieving and substituting individual slots.
+#'
+#' The values passed for replacement need to be of the class of that slot.
+#'
+#' @section Constructor:
+#' 
+#' A construction method exists for this class that can take named arguments for
+#' any of its slots. All slots are then created to match the requirements of the
+#' class validity.
+#'
+#' @author Iago Mosqueira, Finlay Scott - EC JRC.
+#' @seealso \link{fwdControl}
+#' @keywords classes
+#' @examples
+#'
+#' # CREATE targets on fishing mortality ('f') by year
+#'
+#' element <- data.frame(year=2000:2010, value=rlnorm(11), quantity='f')
+#'
+#' fwe <- fwdElement(element=element)
+#'
+#' # INSPECT fwdControl object
+#'
+#' show(fwe)
+
+
 setClass('fwdElement',
 
-	# REPRESENTATION
-	representation(
-		element='data.frame',
-		iters='array'),
+  # REPRESENTATION
+  representation(
+    element='data.frame',
+    iters='array'),
 
-	# PROTOTYPE
-	prototype(
-		element=data.frame(year=1, quantity=factor(NA, levels=FLasher:::qlevels),
-			min=as.numeric(NA), value=0, max=as.numeric(NA), season='all',
-			area='unique', unit='all', relYear=as.integer(NA), relSeason=as.integer(NA), 
-			#relArea='NA', relUnit='NA',
+  # PROTOTYPE
+  prototype(
+    element=data.frame(year=1, quantity=factor(NA, levels=FLasher:::qlevels),
+      min=as.numeric(NA), value=0, max=as.numeric(NA), season='all',
+      area='unique', unit='all', relYear=as.integer(NA), relSeason=as.integer(NA), 
+      #relArea='NA', relUnit='NA',
       stringsAsFactors=FALSE),
-		iters=array(NA, dimnames=list(row=1, val=c('min', 'value', 'max'), iter=1),
-			dim=c(1,3,1))),
+    iters=array(NA, dimnames=list(row=1, val=c('min', 'value', 'max'), iter=1),
+      dim=c(1,3,1))),
 
-	# VALIDITY
-	validity=function(object) {
+  # VALIDITY
+  validity=function(object) {
 
-		# rows in element == rows in iters
-		if(nrow(object@element) != dim(object@iters)[1])
-			return("Mismatch in number of iters in element and array")
-		
-		# if value, no min/max,
-		if(all(is.na(object@element[,'value'])) &
-			 any(is.na(object@element[,'max']), is.na(object@element[,'min'])))
-			return("Only value or min/max")
-		
-		# and viceversa
-		if(any(is.na(object@element[,'value'])) &
-			 all(is.na(object@element[,'max']), is.na(object@element[,'min'])))
-			return("Only value or min/max")
-		
-		# TODO: classes of data.frame columns
+    # rows in element == rows in iters
+    if(nrow(object@element) != dim(object@iters)[1])
+      return("Mismatch in number of iters in element and array")
+    
+    # if value, no min/max,
+    if(all(is.na(object@element[,'value'])) &
+       any(is.na(object@element[,'max']), is.na(object@element[,'min'])))
+      return("Only value or min/max")
+    
+    # and viceversa
+    if(any(is.na(object@element[,'value'])) &
+       all(is.na(object@element[,'max']), is.na(object@element[,'min'])))
+      return("Only value or min/max")
+    
+    # TODO: classes of data.frame columns
 
-		# levels in 'quantity'
-		if(!all(as.character(object@element$quantity) %in% FLasher:::qlevels))
-			return("Specified 'quantity' not available in fwd")
-	}
+    # levels in 'quantity'
+    if(!all(as.character(object@element$quantity) %in% FLasher:::qlevels))
+      return("Specified 'quantity' not available in fwd")
+  }
 ) # }}}
 
 # fwdControl class {{{
@@ -106,6 +156,6 @@ setClass('fwdElement',
 #' show(fwc)
 
 setClass('fwdControl', representation(
-	target='fwdElement'),
-	prototype(target=new('fwdElement')))
+  target='fwdElement'),
+  prototype(target=new('fwdElement')))
 # }}}
--- R/constructors.R
+++ R/constructors.R
@@ -8,248 +8,248 @@
 
 # fwdElement(element='data.frame', iters='array') {{{
 setMethod('fwdElement', signature(element='data.frame', iters='array'),
-	function(element, iters) {
+  function(element, iters) {
 
-		# COMPLETE df
-		ele <- new('fwdElement')@element[rep(1, nrow(element)),]
-		# HACK: drop rownames
-		rownames(ele) <- NULL
-		# CONVERT year
-		if('year' %in% names(element))
-			element$year <- as.integer(element$year)
-		# assign
-		ele[,names(element)] <- element
-		# HACK: reassign quantity to keep factors
-		ele[,'quantity']  <- factor(element$quantity, levels=FLasher:::qlevels)
+    # COMPLETE df
+    ele <- new('fwdElement')@element[rep(1, nrow(element)),]
+    # HACK: drop rownames
+    rownames(ele) <- NULL
+    # CONVERT year
+    if('year' %in% names(element))
+      element$year <- as.integer(element$year)
+    # assign
+    ele[,names(element)] <- element
+    # HACK: reassign quantity to keep factors
+    ele[,'quantity']  <- factor(element$quantity, levels=FLasher:::qlevels)
 
-		# COMPLETE iters
-		dit <- dim(iters)
-		dni <- dimnames(iters)
-		# val
-		if(dit[2] < 3) {
-			ite <- array(NA, dim=c(dit[1], 3, dit[3]), dimnames=list(row=dni[[1]],
-				val=c('min', 'value', 'max'), iter=1:dit[3]))
-			ite[,dni[[2]],] <- iters
-			iters <- ite
-		}
-		# row
-		if(dim(iters)[1] < dim(ele)[1]) {
-			dmn <- dimnames(iters)
-			dmn[['row']] <- seq(1, dim(ele)[1])
-			ite <- array(NA, dimnames=dmn, dim=unlist(lapply(dmn, length)))
-			
-			ite[dni[['row']],,]	<- iters
-	
-			# FIND missing rows (mro)
-			mro <- dmn[['row']][!dmn[['row']] %in% dni[['row']]]
-			ite[mro,'value',]	<- rep(ele[mro, 'value'], dit[3])
-			ite[mro,'min',] <- rep(ele[mro, 'min'], dit[3])
-			ite[mro,'max',] <- rep(ele[mro, 'max'], dit[3])
-			iters <- ite
-		}
-		return(new('fwdElement', element=ele, iters=iters))
-	}
+    # COMPLETE iters
+    dit <- dim(iters)
+    dni <- dimnames(iters)
+    # val
+    if(dit[2] < 3) {
+      ite <- array(NA, dim=c(dit[1], 3, dit[3]), dimnames=list(row=dni[[1]],
+        val=c('min', 'value', 'max'), iter=1:dit[3]))
+      ite[,dni[[2]],] <- iters
+      iters <- ite
+    }
+    # row
+    if(dim(iters)[1] < dim(ele)[1]) {
+      dmn <- dimnames(iters)
+      dmn[['row']] <- seq(1, dim(ele)[1])
+      ite <- array(NA, dimnames=dmn, dim=unlist(lapply(dmn, length)))
+      
+      ite[dni[['row']],,]  <- iters
+  
+      # FIND missing rows (mro)
+      mro <- dmn[['row']][!dmn[['row']] %in% dni[['row']]]
+      ite[mro,'value',]  <- rep(ele[mro, 'value'], dit[3])
+      ite[mro,'min',] <- rep(ele[mro, 'min'], dit[3])
+      ite[mro,'max',] <- rep(ele[mro, 'max'], dit[3])
+      iters <- ite
+    }
+    return(new('fwdElement', element=ele, iters=iters))
+  }
 ) # }}}
 
 # fwdElement(element='data.frame', iters='matrix') {{{
 setMethod('fwdElement', signature(element='data.frame', iters='matrix'),
-	function(element, iters) {
+  function(element, iters) {
 
-		dni <- dimnames(iters)
+    dni <- dimnames(iters)
 
-		# NO dimnames in iters, assume dims are 'row' & 'iter' for 'value'
-		if(is.null(dni)) {
-			dimnames(iters) <- list(row=1:dim(iters)[1], iter=1:dim(iters)[2])
-			dni <- dimnames(iters)
-		}
+    # NO dimnames in iters, assume dims are 'row' & 'iter' for 'value'
+    if(is.null(dni)) {
+      dimnames(iters) <- list(row=1:dim(iters)[1], iter=1:dim(iters)[2])
+      dni <- dimnames(iters)
+    }
 
-		dms <- list(row=1, val=c('min', 'value', 'max'), iter=1)
-		dms[names(dni)] <- dni
+    dms <- list(row=1, val=c('min', 'value', 'max'), iter=1)
+    dms[names(dni)] <- dni
 
-		ite <- array(NA, dimnames=dms, dim=unlist(lapply(dms, length)))
+    ite <- array(NA, dimnames=dms, dim=unlist(lapply(dms, length)))
 
-		# MISSING 'val' dimension, assume val='value'
-		if(!"val" %in% names(dni)) {
-			ite[,'value',] <- iters
-		}
-		# MISSING row
-		else if (!"row" %in% names(dni)) {
-			ite[1,,] <- iters
-		}
-		# MISSING iter
-		if(!"iter" %in% names(dni))
-			stop("No 'iter' dimname in iters, cannot create object")
+    # MISSING 'val' dimension, assume val='value'
+    if(!"val" %in% names(dni)) {
+      ite[,'value',] <- iters
+    }
+    # MISSING row
+    else if (!"row" %in% names(dni)) {
+      ite[1,,] <- iters
+    }
+    # MISSING iter
+    if(!"iter" %in% names(dni))
+      stop("No 'iter' dimname in iters, cannot create object")
 
-		return(fwdElement(element=element, iters=ite))
-	}
+    return(fwdElement(element=element, iters=ite))
+  }
 ) # }}}
 
 # fwdElement(element='data.frame', iters='numeric') {{{
 setMethod('fwdElement', signature(element='data.frame', iters='numeric'),
-	function(element, iters) {
-	
-		# CREATE iters
-		dti <- dim(element)
-		ite <- array(NA, dim=c(dti[1], 3, iters), dimnames=list(row=1:dti[1], 
-			val=c('min', 'value', 'max'), iter=seq(iters)))
+  function(element, iters) {
+  
+    # CREATE iters
+    dti <- dim(element)
+    ite <- array(NA, dim=c(dti[1], 3, iters), dimnames=list(row=1:dti[1], 
+      val=c('min', 'value', 'max'), iter=seq(iters)))
 
-		# FIND val names in element
-		vns <- c('min', 'value', 'max')
-		nms <- vns %in% colnames(element)
+    # FIND val names in element
+    vns <- c('min', 'value', 'max')
+    nms <- vns %in% colnames(element)
 
         # ASSIGN values if 'value', 'min' or 'max' in df colnames
         if(any(nms))
-    		ite[, vns[nms],] <- element[,vns[nms]]
+        ite[, vns[nms],] <- element[,vns[nms]]
 
-		return(fwdElement(element=element, iters=ite))
-	}
+    return(fwdElement(element=element, iters=ite))
+  }
 )
 # }}}
 
 # fwdElement(element='data.frame', iters='missing') {{{
 setMethod('fwdElement', signature(element='data.frame', iters='missing'),
-	function(element) {
-		
-		# CREATE iters
-		dti <- dim(element)
-		ite <- array(NA, dim=c(dti[1], 3, 1), dimnames=list(row=1:dti[1], 
-			val=c('min', 'value', 'max'), iter=1))
+  function(element) {
+    
+    # CREATE iters
+    dti <- dim(element)
+    ite <- array(NA, dim=c(dti[1], 3, 1), dimnames=list(row=1:dti[1], 
+      val=c('min', 'value', 'max'), iter=1))
 
-		# FIND val names in element
-		vns <- c('min', 'value', 'max')
-		nms <- vns %in% colnames(element)
-		ite[, vns[nms], 1] <- unlist(c(element[,vns[nms]]))
+    # FIND val names in element
+    vns <- c('min', 'value', 'max')
+    nms <- vns %in% colnames(element)
+    ite[, vns[nms], 1] <- unlist(c(element[,vns[nms]]))
 
-		return(fwdElement(element=element, iters=ite))
-	}
+    return(fwdElement(element=element, iters=ite))
+  }
 )
 # }}}
 
 # fwdElement(element='list', iters='missing') {{{
 setMethod('fwdElement', signature(element='list', iters='missing'),
-	function(element) {
+  function(element) {
 
-	foo <- function(...) {
+  foo <- function(...) {
 
-		args <- list(...)
-	
-		# SEPARATE df and
-		df <- as.data.frame(args[!names(args) %in% 'value'])
-		
-		# array components
-		val <- args[['value']]
-		
-		# TURN val into matrix
-		if(is(val, 'list')) {
-			mat <- do.call(rbind, val)
-		} else {
-			mat <- t(matrix(val))
-		}
-		# CHECK no. rows in iters
-		if(nrow(df) > nrow(mat)) {
-			mat <- t(matrix(mat, nrow=length(mat), ncol=nrow(df)))
-		}
+    args <- list(...)
+  
+    # SEPARATE df and
+    df <- as.data.frame(args[!names(args) %in% 'value'])
+    
+    # array components
+    val <- args[['value']]
+    
+    # TURN val into matrix
+    if(is(val, 'list')) {
+      mat <- do.call(rbind, val)
+    } else {
+      mat <- t(matrix(val))
+    }
+    # CHECK no. rows in iters
+    if(nrow(df) > nrow(mat)) {
+      mat <- t(matrix(mat, nrow=length(mat), ncol=nrow(df)))
+    }
 
-		ele <- new('fwdElement')@element[rep(1, 1),]
-		ele[names(df)] <- df
+    ele <- new('fwdElement')@element[rep(1, 1),]
+    ele[names(df)] <- df
 
-		return(list(element=ele, iters=mat))
-	}
-	
-	if(is(element[[1]], 'list')) {
-		
-		inp <- lapply(element, function(x) do.call('foo', x))
+    return(list(element=ele, iters=mat))
+  }
+  
+  if(is(element[[1]], 'list')) {
+    
+    inp <- lapply(element, function(x) do.call('foo', x))
 
-		ele <- do.call('rbind', lapply(inp, function(x) x$element))
-		ite <- do.call('rbind', lapply(inp, function(x) x$iters))
+    ele <- do.call('rbind', lapply(inp, function(x) x$element))
+    ite <- do.call('rbind', lapply(inp, function(x) x$iters))
 
-		return(fwdElement(element=ele, iters=ite))
+    return(fwdElement(element=ele, iters=ite))
 
-	} else {
-		
-		inp <- do.call('foo', element)
-		return(do.call('fwdElement', inp))
-	}
-	
-		
-	}
+  } else {
+    
+    inp <- do.call('foo', element)
+    return(do.call('fwdElement', inp))
+  }
+  
+    
+  }
 )
 
 # }}}
 
 # fwdElement(element='missing', iters='missing') {{{
 setMethod('fwdElement', signature(element='missing', iters='missing'),
-	function(...) {
+  function(...) {
 
-		args <- list(...)
-		
-		# SEPARATE df and
-		df <- as.data.frame(args[!names(args) %in% 'value'])
-		
-		# array components
-		val <- args[['value']]
-		
-		# TURN val into matrix
-		if(is(val, 'list')) {
-			mat <- do.call(rbind, val)
-		} else {
-			mat <- t(matrix(val))
-		}
-		# CHECK no. rows in iters
-		if(nrow(df) > nrow(mat)) {
-			mat <- t(matrix(mat, nrow=length(mat), ncol=nrow(df)))
-		}
-		return(fwdElement(element=df, iters=mat))
-	}
+    args <- list(...)
+    
+    # SEPARATE df and
+    df <- as.data.frame(args[!names(args) %in% 'value'])
+    
+    # array components
+    val <- args[['value']]
+    
+    # TURN val into matrix
+    if(is(val, 'list')) {
+      mat <- do.call(rbind, val)
+    } else {
+      mat <- t(matrix(val))
+    }
+    # CHECK no. rows in iters
+    if(nrow(df) > nrow(mat)) {
+      mat <- t(matrix(mat, nrow=length(mat), ncol=nrow(df)))
+    }
+    return(fwdElement(element=df, iters=mat))
+  }
 )
 
 # }}}
 
 # fwdControl(target=fwdElement, iters='missing') {{{
 setMethod('fwdControl', signature(target='fwdElement', iters='missing'),
-	function(target) {
-		return(new('fwdControl', target=target))
-	}
+  function(target) {
+    return(new('fwdControl', target=target))
+  }
 ) # }}}
 
 # fwdControl(target='data.frame', iters='array') {{{
 setMethod('fwdControl', signature(target='data.frame', iters='array'),
-	function(target, iters) {
+  function(target, iters) {
 
-		ele <- fwdElement(element=target, iters=iters)
+    ele <- fwdElement(element=target, iters=iters)
 
-		return(new("fwdControl", target=ele))
-	}
+    return(new("fwdControl", target=ele))
+  }
 ) # }}}
 
 # fwdControl(target='data.frame', iters='matrix') {{{
 setMethod('fwdControl', signature(target='data.frame', iters='matrix'),
-	function(target, iters) {
-		
-		ele <- fwdElement(element=target, iters=iters)
+  function(target, iters) {
+    
+    ele <- fwdElement(element=target, iters=iters)
 
-		return(new("fwdControl", target=ele))
-	}
+    return(new("fwdControl", target=ele))
+  }
 ) # }}}
 
 # fwdControl(target='data.frame', iters='numeric') {{{
 setMethod('fwdControl', signature(target='data.frame', iters='numeric'),
-	function(target, iters) {
-		
-		ele <- fwdElement(element=target, iters=iters)
+  function(target, iters) {
+    
+    ele <- fwdElement(element=target, iters=iters)
 
-		return(new("fwdControl", target=ele))
-	}
+    return(new("fwdControl", target=ele))
+  }
 )
 # }}}
 
 # fwdControl(target='data.frame', iters='missing') {{{
 setMethod('fwdControl', signature(target='data.frame', iters='missing'),
-	function(target) {
-		
-		ele <- fwdElement(element=target)
+  function(target) {
+    
+    ele <- fwdElement(element=target)
 
-		return(new("fwdControl", target=ele))
-	}
+    return(new("fwdControl", target=ele))
+  }
 )
 # }}}
