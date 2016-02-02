361025cf4289566b339c1320d276886ab4c9c19e
Iago Mosqueira <iago@mosqueira.eu>
2016-02-02 14:41:56 +0100

    validity of fwdControl checks for min/max and value

--- R/classes.R
+++ R/classes.R
@@ -92,6 +92,7 @@ setClass('fwdControl',
     # value & min/max
     idx <- !is.na(object@iters)
     ids <- idx[,1,] + idx[,3,] + (10 * idx[,2,])
+    ids <- idx[,1,,drop=FALSE] + idx[,3,,drop=FALSE] + (10 * idx[,2,,drop=FALSE])
     rsu <- rowSums(ids / dim(idx)[3])
 
     if(any(rsu == 11 | rsu == 22))
