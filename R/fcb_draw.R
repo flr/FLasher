390890710e8f877695d340bf222740278192e859
Finlay Scott <finlay.scott@jrc.ec.europa.eu>
2017-12-13 16:34:30 +0100

    Fix check

--- R/fcb_draw.R
+++ R/fcb_draw.R
@@ -456,7 +456,7 @@ jukebox <- function(){
     return(tracks[[trackno]][lyricno])
 }
 
-.onLoad <- function(libname, pkgname ) {
+.onAttach <- function(libname, pkgname ) {
   packageStartupMessage(paste(jukebox(),"\n",sep=""))
 }
 
