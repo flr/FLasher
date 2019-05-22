9e0e35c4a72513b9972e8395a2bb31cc02c078e3
Iago Mosqueira <iago@mosqueira.eu>
2019-05-22 21:36:49 +0200

    Left behind browser deleted

--- R/fwd.R
+++ R/fwd.R
@@ -234,7 +234,6 @@ setMethod("fwd", signature(object="FLBiols", fishery="FLFisheries", control="fwd
   # CALL operatingModelRun
   out <- operatingModelRun(rfishery, biolscpp, control, effort_max=effort_max,
     effort_mult_initial = 1.0, indep_min = 1e-6, indep_max = 1e12, nr_iters = 50)
-  browser()
  
   # STRUCTURE of out
   #
