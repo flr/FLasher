a6ffee9b9b6f46dd4263cbaf605399c886b504f9
Iago Mosqueira <iago@mosqueira.eu>
2021-03-18 07:47:38 +0100

    plot(FLStocks, fwdControl)

--- R/plot.R
+++ R/plot.R
@@ -95,3 +95,41 @@ setMethod("plot", signature(x="FLQuants", y="fwdControl"),
         fill = fill, alpha = 0.1)
   }
 ) # }}}
+
+# plot(FLStocks, fwdControl) {{{
+
+#' plot method for FLStocks, fwdControl
+#'
+#' @name plot
+#' @aliases plot,FLStocks,fwdControl-method
+#' @docType methods
+#' @section Generic function: plot(x, y)
+#' @param x FlStocks object to plot
+#' @param y fwdControl from which to extract year ranges
+#' @param fill Colour to fill projection years background
+#' @param ... Any other argument to be passed to [ggplotFL::plot]
+#' @examples
+#' data(ple4)
+#' control <- fwdControl(year=2008:2017, quant="f", value=0.3)
+#' # No fwd projection took place, simply passing year range
+#' plot(FLStocks(PLE4=ple4), control)
+
+setMethod("plot", signature(x="FLStocks", y="fwdControl"),
+  function(x, y, fill="#E69F00", ...) {
+
+    yrs <- range(y$year)
+
+    # CREATE standard plot
+    p <- plot(x, ...)
+
+    # GET x variable
+    if(as_name(p$mapping$x) == "date") {
+      yrs <- range(p$data[with(p$data, year %in% yrs), "date"])
+    }
+
+    p + geom_vline(xintercept=yrs[1], alpha=0.4) +
+      geom_vline(xintercept=yrs[2], alpha=0.2) +
+      annotate("rect", xmin = yrs[1], xmax = yrs[2], ymin = -Inf, ymax = Inf,
+        fill = fill, alpha=0.1)
+  }
+) # }}}
