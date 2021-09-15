c20dfb1ab8d176f76871ef8e2c1a4587285bf026
Iago Mosqueira <iago@mosqueira.eu>
2021-09-15 12:00:31 +0200

    Changed tolerance to match all.equal's default: 1.5e-8

--- inst/include/solver.h
+++ inst/include/solver.h
@@ -12,4 +12,4 @@ double euclid_norm(std::vector<double> x);
 // A Newton Raphson solver for a function that has already been taped.
 // Pass in the independent variables, tape no. and control parameters
 // int newton_raphson(std::vector<double>& indep, const int adolc_tape, const int max_iters= 50, const double max_limit = 100, const double tolerance = 1e-12);
-std::vector<int> newton_raphson(std::vector<double>& indep, CppAD::ADFun<double>& fun, const unsigned int niter, const unsigned int nsim_targets, const double indep_min = 0, const double indep_max = 1e9, const unsigned int max_iters= 50, const double tolerance = 1e-12);
+std::vector<int> newton_raphson(std::vector<double>& indep, CppAD::ADFun<double>& fun, const unsigned int niter, const unsigned int nsim_targets, const double indep_min = 0, const double indep_max = 1e9, const unsigned int max_iters= 50, const double tolerance = 1.5e-8);
