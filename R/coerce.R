a80828689aae8b3cf3a3ae43d222d5a8c4055674
shfischer <32540427+shfischer@users.noreply.github.com>
2021-11-11 11:13:10 +0000

    fix bug when seasons provided in FLQuant - do not drop seasons

--- R/coerce.R
+++ R/coerce.R
@@ -117,7 +117,7 @@ setAs("FLQuants", "fwdControl",
     # ITERS
 		} else {
 
-			target <- cbind(df[df$iter == df$iter[1],][,c('year', 'quant')])
+			target <- cbind(df[df$iter == df$iter[1],][,c('year', 'season', 'quant')])
 	
       # ARRAY iters [targets, 3, iters]    
       iters <- array(NA, dim=c(dim(target)[1], 3, its),
