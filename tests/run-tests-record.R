7eba71c045e7a90eecf08cc8ce21e5a16d1b2adb
Iago Mosqueira <iagomosqueira@users.noreply.github.com>
2026-05-15 17:23:17 +0200

    Revert "Add comprehensive per-source `testthat` coverage and output-recording runner for FLasher"

--- tests/run-tests-record.R
+++
@@ -1,6 +0,0 @@
-if (identical(Sys.getenv("FLASHER_RECORD_TEST_OUTPUT"), "true")) {
-  library(testthat)
-
-  out <- test_dir("testthat", reporter = "summary")
-  saveRDS(out, file = file.path("tests", "test-results.rds"))
-}
